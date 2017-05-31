87c590841c76a744aa03e33890f1addf92ffdf6a
Tony133 <a.tripodi133@gmail.com>
2017-05-31 17:18:20 +0200

    Update

--- web/app.php
+++ web/app.php
@@ -5,8 +5,10 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * @var Composer\Autoload\ClassLoader
  */
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
+$loader = require __DIR__.'/../vendor/autoload.php';
+if (PHP_VERSION_ID < 70000) {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
+}
 
 $kernel = new AppKernel('prod', false);
 if (PHP_VERSION_ID < 70000) {
